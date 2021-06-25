e4575c7e4342933bc02714c2cad39d1ef3e36f1b
nokindj <nokindj@gmail.com>
2021-06-25 11:00:59 +0100

    added dummy data

---
+++ src/components/layout/SignedInLinks.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+const SignedInLinks = () => {
+  return (
+    <ul className='right'>
+      <li>
+        <NavLink to='/create' className=''>
+          New Project
+        </NavLink>
+      </li>
+      <li>
+        <NavLink to='/' className=''>
+          Log Out
+        </NavLink>
+      </li>
+      <li>
+        <NavLink to='/' className='btn btn-floating pink lighten-1'>
+          NN
+        </NavLink>
+      </li>
+    </ul>
+  );
+};
+
+export default SignedInLinks;
